5c06cf6138e7ec33eb6719466ebdbc79a08c5bf6
NammuLovett <nammmu@gmail.com>
2023-05-08 12:41:13 +0200

    config db mapin

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Database connection values */
+define("DB_HOST", "localhost");
+define("DB", "mapin");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+
+/* Default options */
+
+define("DEFAULT_ACTION", "web");
