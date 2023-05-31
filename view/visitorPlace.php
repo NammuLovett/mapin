438b83717a4c9a23b7afafc7b479cc9b6065c21c
NammuLovett <nammmu@gmail.com>
2023-05-31 12:53:19 +0200

    .`ñace

--- view/visitorPlace.php
+++ view/visitorPlace.php
@@ -1,7 +1,6 @@
 <?php
 $mapin = $_SESSION['mapin'];
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -83,18 +82,22 @@ $mapin = $_SESSION['mapin'];
             <div class="place-image">
                 <img src="zimg/places/<?php echo $mapin->getImgPlace(); ?>" alt="Imagen del lugar" srcset="" class="place-image-img">
             </div>
-
+            <!-- Comprueba si la variable de sesión $mapin está definida y no es NULL -->
             <?php if ($mapin) : ?>
                 <div class="place-details">
                     <div class="icon-links-container">
                         <p id="mensaje"></p>
 
                         <!-- VISITADO -->
+                        <!--  Marcar/desmarcar un lugar como visitado -->
                         <a href="index.php?action=toggleVisited&idPlace=<?php echo $idPlace; ?>" class="icon-link <?php echo $hasVisited ? 'activo' : ''; ?>" id="link-visitado" data-place="<?php echo $idPlace; ?>" onclick="toggleVisited(event)">
                             <div class="circle">
+                                <!-- Si el lugar ha sido visitado muestra un icono ✅, de lo contrario, muestra una ❌ -->
                                 <i class="fas fa-<?php echo $hasVisited ? 'check' : 'times'; ?>"></i>
                             </div>
+                            <!-- Muestra 'Visitado' si el lugar ha sido visitado, sino, muestra 'No visitado' -->
                             <span><?php echo $hasVisited ? 'Visitado' : 'No visitado'; ?></span>
+                            <!-- Si se ha visitado, muestra la fecha que visitó el lugar-->
                             <?php if ($hasVisited) : ?>
                                 <span data-date style="display: inline;"><?php echo date('d/m/Y', strtotime($visitDate)); ?></span>
                             <?php else : ?>
@@ -102,19 +105,18 @@ $mapin = $_SESSION['mapin'];
                             <?php endif; ?>
                         </a>
 
-
-
-
                         <!-- FAVORITO -->
+                        <!--  marcar/desmarcar un lugar como Favorito -->
                         <a href="index.php?action=toggleFavorited&idPlace=<?php echo $idPlace; ?>" class="icon-link <?php echo $isFavorited ? 'activo' : ''; ?>" id="link-favorito" data-place="<?php echo $idPlace; ?>" onclick="toggleFavorited(event)">
                             <div class="circle">
+                                <!-- Icono de estrella -->
                                 <i class="fas fa-star"></i>
                             </div>
                             <span>Favorito</span>
                         </a>
+                    </div><!-- Fin de icon-links-container -->
 
-                    </div>
-
+                    <!-- Bloque de información del lugar -->
                     <div class="place-info">
                         <h1 class="place-title"><?php echo $mapin->getNamePlace(); ?></h1>
                         <p class="place-location"><i class="fa-solid fa-location-dot"></i> Ceuta, España</p>
@@ -123,12 +125,12 @@ $mapin = $_SESSION['mapin'];
                         <h2 class="description-title">Descripción</h2>
                         <p><?php echo $mapin->getDescriptionPlace(); ?></p>
                     </div>
+                    <!-- DIV Mapa del Lugar -->
                     <div id="map" style="height: 400px; width: 100%;"></div>
                 </div>
             <?php endif; ?>
-
-
         </section>
+
         <!-- Tercera columna: Perfil y lugares visitados -->
         <section class="columna-3">
             <!-- c3 :Primera fila -->
@@ -156,12 +158,13 @@ $mapin = $_SESSION['mapin'];
 
         </section>
     </main>
-
+    <!-- Api KEY google -->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCJXT7vkQCPszRpdMfAJO7hMr55J31aZug&libraries=geometry&callback=initMap" async defer></script>
-    <!-- Mapita -->
+
+    <!-- Mapa Google mostrando el lugar vista detalle PLACE -->
     <script>
+        // Función para inicializar el mapa
         function initMap() {
-            // Define la ubicación del lugar con latitud y longitud
             var placeLocation = {
                 lat: <?php echo $mapin->getLatPlace(); ?>,
                 lng: <?php echo $mapin->getLonPlace(); ?>
@@ -190,19 +193,19 @@ $mapin = $_SESSION['mapin'];
             // Define la ventana de información inicialmente como null
             var infoWindow = null;
 
-            // Verifica si la geolocalización está habilitada
+            // Comprueba si la geolocalización está habilitada en el navegador del usuario
             if (navigator.geolocation) {
                 var visitedLink = document.getElementById('link-visitado');
                 var visitedSpan = visitedLink.querySelector('span');
 
-                // Vigila la posición del usuario y actualiza el mapa y la ventana de información en consecuencia
+                // Posición del usuario, actualiza el mapa y la ventana de información
                 var watchID = navigator.geolocation.watchPosition(function(position) {
                     var userLocation = {
                         lat: position.coords.latitude,
                         lng: position.coords.longitude
                     };
 
-                    // Crea un marcador para la ubicación del usuario
+                    // Crea un marcador en la ubicación actual del usuario
                     var userMarker = new google.maps.Marker({
                         position: userLocation,
                         map: map,
@@ -218,7 +221,7 @@ $mapin = $_SESSION['mapin'];
                     var placeLatLng = new google.maps.LatLng(placeLocation.lat, placeLocation.lng);
                     var distance = google.maps.geometry.spherical.computeDistanceBetween(userLatLng, placeLatLng);
 
-                    // Decide cómo se muestra la distancia dependiendo de si es más o menos de 1 km
+                    // Formato de medida dependiendo de la distancia (si es más o menos de 1 km)
                     var displayDistance = distance < 1000 ? distance.toFixed(2) + " metros" : (distance / 1000).toFixed(2) + " km";
 
                     // Define el contenido de la ventana de información
@@ -250,7 +253,7 @@ $mapin = $_SESSION['mapin'];
                         visitedLink.classList.remove('lejos');
                         visitedSpan.innerText = visitedLink.classList.contains('activo') ? 'Visitado' : 'No visitado';
                     }
-
+                    // Si ocurre un error al obtener la ubicación del usuario, muestra un mensaje de alerta
                 }, function() {
                     alert('Error: El navegador no permite la geolocalización, o esta ha sido desactivada.');
                 });
@@ -261,16 +264,13 @@ $mapin = $_SESSION['mapin'];
                         infoWindow.close();
                     }
                 });
+                // Si la geolocalización no está soportada en el navegador del usuario, muestra un mensaje de alerta
             } else {
                 alert('Error: Tu navegador no soporta la geolocalización.');
             }
         }
     </script>
-
-
     <script src="view/js/visitor.js"></script>
-
-
 </body>
 
 </html>
